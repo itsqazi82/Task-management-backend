80c4b302303ed81b55f4ef899e068a0b3f824c50
MrSohaibAhmed <129491757+MrSohaibAhmed@users.noreply.github.com>
2025-10-31 22:44:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,6 @@ app.post('/api/auth/login', login);
 app.use('/api/admin', adminRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

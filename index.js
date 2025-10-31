16424d9cdef0a87d7244678dd30ce7494f3283f2
MrSohaibAhmed <129491757+MrSohaibAhmed@users.noreply.github.com>
2025-10-31 23:27:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ const allowedOrigins = [
   "https://task-management-frontend-three-gamma.vercel.app",
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(cors());
 
 // ✅ Handle preflight
 // ✅ Handle preflight properly for Express v5
